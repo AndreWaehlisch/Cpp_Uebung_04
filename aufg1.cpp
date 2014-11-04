43ea058d44b1bf80ced97e03690b1eb3e28274ac
AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>
2014-11-04 07:25:17 +0100

    aufg1
    Signed-off-by: AndreWaehlisch <andre.waehlisch@physik.tu-berlin.de>

--- aufg1.cpp
+++ aufg1.cpp
@@ -2,23 +2,49 @@
 #include <cmath>
 using namespace std;
 
-int bracket(Functor & func, double &a, double &b)
+const int MAXSTEPS = 100;
+
+class MyFunc {
+ public:
+
+	double a;
+
+	 MyFunc(double init) {
+		a = init;
+	} double operator() (double x) {
+		return (sinh(x) / x - (a * x * x));
+	}
+};
+
+int bracket(MyFunc & func, double &a, double &b)
 {
 	double f = func(a);
 	double g = func(b);
 
-	for (int i = 1; i <= 100; i++) {
+	for (int i = 1; i <= MAXSTEPS; i++) {
 		if (f * g < 0.)
 			return i;
 
 		if (abs(f) < abs(g))
 			f = func(a += 1.1 * (a - b));
 		else
-			g = func(b += 1.1 * (b - 1));
+			g = func(b += 1.1 * (b - a));
 	}
+
+	return 0;
 }
 
 int main()
 {
+	MyFunc test(4);
 
+	double x1 = 6.1;
+	double x2 = 6.2;
+
+	int steps = bracket(test, x1, x2);
+
+	if (steps == 0)
+		cout << "Calculation did not finish after " << MAXSTEPS << " steps!" << endl;
+	else
+		cout << "Calculation took " << steps << " steps.";
 }
